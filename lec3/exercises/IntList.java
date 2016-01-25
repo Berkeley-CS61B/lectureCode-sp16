4da4ea20405aba1d3879c3186f17307bdcf195ed
Josh Hug <hug@cs.berkeley.edu>
2016-01-25 14:37:05 -0800

    removed intlistops class to make things easier to put through the visualizer

--- lec3/exercises/IntList.java
+++ lec3/exercises/IntList.java
@@ -44,11 +44,33 @@ public class IntList {
         return Integer.toString(head) + " " + tail.toString();
     }
 
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. L is not allowed
+      * to change. */
+    public static IntList incrList(IntList L, int x) {
+        /* Your code here. */
+        return L;        
+    }
+
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. Not allowed to use
+      * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+        /* Your code here. */
+        return L;
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(5, null);
         L.tail = new IntList(7, null);
+        L.tail.tail = new IntList(9, null);
+
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
-        System.out.println(L.get(1));
+
+        // Test your answers by uncommenting. Or use the Visualizer.
+        // System.out.println(L.get(1));
+        // System.out.println(incrList(L, 3));
+        // System.out.println(dincrList(L, 3));        
     }
 } 
--- lec3/exercises/hardMode/IntList.java
+++ lec3/exercises/hardMode/IntList.java
@@ -12,12 +12,14 @@ public class IntList {
 
     /** Retuns the size of this IntList */
     public int size() {
-        /* your code here */        
+        /* your code here */       
+        return 0; 
     }
 
     /** Returns the size of this IntList. */
     public int iterativeSize() {
         /* your code here */
+        return 0;
     }
 
     /** In class exercise 1: 
@@ -28,15 +30,40 @@ public class IntList {
         /** your code here */
     }
 
+    /** Returns the Intlist as a string. Automatically called
+      * whenever you try to print an IntList. */
     public String toString() {
         /* your code here */
+        return null;
     }
 
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. L is not allowed
+      * to change. */
+    public static IntList incrList(IntList L, int x) {
+        /* Your code here. */
+        return L;        
+    }
+
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. Not allowed to use
+      * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+        /* Your code here. */
+        return L;
+    }    
+
     public static void main(String[] args) {
-        IntList L = new IntList(5, null);
-        L.tail = new IntList(7, null);
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
-        System.out.println(L.get(1));
+
+        // Test your answers by uncommenting. Or use the Visualizer.
+        // IntList L = new IntList(5, null);
+        // L.tail = new IntList(7, null);    
+        // L.tail.tail = new IntList(9, null);
+        // System.out.println(L.size());
+        // System.out.println(L.iterativeSize());        
+        // System.out.println(L.get(1));
+        // System.out.println(incrList(L, 3));
+        // System.out.println(dincrList(L, 3));  
+
     }
 } 
