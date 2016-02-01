fb6381d4b7967a8461a4fd424f1a891ff591fe74
Josh hug <hug@cs.berkeley.edu>
2016-02-01 11:33:01 -0800

    lec6 code

--- lec6/webvid/AList.java
+++ lec6/webvid/AList.java
@@ -4,9 +4,9 @@
 
 /* The next item ALWAYS goes in the size postiion */
 
-public class AList {
+public class AList<Item> {
 	/* the stored integers */
-	private int[] items;
+	private Item[] items;
 	private int size;
 
 	private static int RFACTOR = 2;
@@ -14,19 +14,19 @@ public class AList {
     /** Creates an empty list. */
     public AList() {
     	size = 0;
-    	items = new int[100];
+    	items = (Item[]) new Object[100];
     }
 
     /** Resize our backing array so that it is
       * of the given capacity. */
     private void resize(int capacity) {
-    	int[] a = new int[capacity];
+    	Item[] a = (Item[]) new Object[capacity];
     	System.arraycopy(items, 0, a, 0, size);
     	items = a;    	
     }
 
     /** Inserts X into the back of the list. */
-    public void insertBack(int x) {    	
+    public void insertBack(Item x) {    	
     	if (size == items.length) {
     		resize(size * RFACTOR);
     	}
@@ -36,12 +36,12 @@ public class AList {
     }
 
     /** Returns the item from the back of the list. */
-    public int getBack() {
+    public Item getBack() {
     	int lastActualItemIndex = size - 1;
     	return items[lastActualItemIndex];
     }
     /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
+    public Item get(int i) {
         return items[i];
     }
 
@@ -52,22 +52,13 @@ public class AList {
 
     /** Deletes item from back of the list and
       * returns deleted item. */
-    public int deleteBack() {
-		int itemToReturn = getBack();
+    public Item deleteBack() {
+		Item itemToReturn = getBack();
 		/* setting to zero not strictly necessary, but
 		 * it's a good habit (we'll see why soon) */
-		items[size - 1] = 0;
+		items[size - 1] = null;
 		size = size - 1;
 		return itemToReturn;
     }
 
-    public static void main(String[] args) {
-    	AList a1 = new AList();
-    	int i = 0;
-    	while (i < 100000000) {
-    		a1.insertBack(i);
-    		i = i + 1;
-    	}
-    	System.out.println("Done!");
-    }
 } 
